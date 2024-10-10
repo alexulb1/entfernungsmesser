6314c732a2294893bee5421f66a4a188c716ca09
alexulb1 <109351274+alexulb1@users.noreply.github.com>
2024-10-10 14:05:15 +0200

    mit taster A und B zum Ein- und Ausschalten und einer Status-LED erweitert

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
 input.onSound(DetectedSound.Loud, function () {
-    aktiv = !(aktiv)
-    if (aktiv) {
-        startzeit = input.runningTime()
-        basic.setLedColor(0x0000ff)
-        basic.pause(500)
-    } else {
-        dauer = input.runningTime() - startzeit
-        basic.turnRgbLedOff()
-        while (true) {
-            basic.showNumber(dauer)
+    if (status == 1) {
+        aktiv = !(aktiv)
+        if (aktiv) {
+            startzeit = input.runningTime()
+            basic.setLedColor(0x0000ff)
+            basic.pause(500)
+        } else {
+            dauer = input.runningTime() - startzeit
+            basic.turnRgbLedOff()
+            while (status == 1) {
+                basic.showNumber(dauer)
+            }
         }
     }
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    status = 1
+    basic.setLedColor(0x00ff00)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    status = 0
+    basic.setLedColor(0xff0000)
+    basic.pause(500)
+    basic.turnRgbLedOff()
 })
 let dauer = 0
 let startzeit = 0
 let aktiv = false
 let status = 0
+status = 0
 input.setSoundThreshold(SoundThreshold.Loud, 128)
 aktiv = false
